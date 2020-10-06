d703ddbe5ddade447de4f8bb47333724647eeff8
Jim Stringer <jim.stringer@gmail.com>
2020-10-06 18:44:25 -0500

    change to HashRouter

--- src/index.js
+++ src/index.js
@@ -4,13 +4,14 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import AuthProvider from './provider/AuthProvider'
-import {BrowserRouter} from 'react-router-dom'
+//import {BrowserRouter} from 'react-router-dom'
+import {HashRouter} from 'react-router-dom'
 ReactDOM.render(
-<BrowserRouter>
+<HashRouter>
   <AuthProvider>
     <App />
   </AuthProvider>
-</BrowserRouter>
+</HashRouter>
 , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
