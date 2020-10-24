3161ab5bdbda9a23d6dbb28604af10e8ae9823c7
Jim Stringer <jim.stringer@gmail.com>
2020-10-23 19:39:29 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/jsbudget-fs/precache-manifest.7b2be10ed8a2d2755881dccd57460cf1.js"
+  "/jsbudget-fs/precache-manifest.577316c1cb01646589252fcd534b3f47.js"
 );
 
 self.addEventListener('message', (event) => {
